88d40eb9213018adf54155914241577f84e65a1f
SEB_FE_41_김소라 <110882351+kimsorara@users.noreply.github.com>
2023-11-04 11:02:37 +0900

    home button

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTree } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <h1 className="header">
-      <span>모</span>
-      <span>여</span>
-      <span>봐</span>
-      <span>요</span>
-      <span>!</span>
-      <span>동</span>
-      <span>물</span>
-      <span>의</span>
-      <span>숲</span>
-      <span>
-        <FontAwesomeIcon icon={faTree} />
-      </span>
-    </h1>
+    <header className="header">
+      <Link to="/">
+        <span>모</span>
+        <span>여</span>
+        <span>봐</span>
+        <span>요</span>
+        <span>!</span>
+        <span>동</span>
+        <span>물</span>
+        <span>의</span>
+        <span>숲</span>
+
+        <span>
+          <FontAwesomeIcon icon={faTree} />
+        </span>
+      </Link>
+    </header>
   );
 };
 
